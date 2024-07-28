ccbda11bfac1a15b446550274b178b82c88d9a73
Кузнецов Вячеслав Олегович <vokuznecov@vtb.ru>
2024-07-28 15:32:01 +0300

    Added file scanner

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,27 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        Scanner scanner = new Scanner(System.in);
+        int count = 1;
+
+        while (true) {
+            System.out.print("Enter path to file: ");
+            File file = new File(scanner.nextLine());
+
+            if (file.exists()) {
+                if (file.isDirectory()) {
+                    System.out.print("This is a directory, not a file. ");
+                    continue;
+                }
+            } else {
+                System.out.print("Incorrect path. ");
+                continue;
+            }
+
+            System.out.println("Correct path. This is file #" + count);
+            count++;
+        }
     }
 }
